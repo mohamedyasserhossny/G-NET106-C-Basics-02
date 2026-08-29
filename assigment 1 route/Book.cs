0c06d9444478d325dc1e70ee3e2b10a7d71b4031
mohamedyasserhossny <my226546@gmail.com>
2026-08-29 13:12:22 -0700

    question1

---
+++ assigment 1 route/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigment_1_route
+{
+    internal class Book
+    {
+        public string title;
+        public int pages;
+        public override string ToString()
+        {
+            return $"book title ={title},book pages={pages}";
+        }
+    }
+}
--- assigment 1 route/Program.cs
+++ assigment 1 route/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            #region question 1
+            Book b1 = new Book();
+            b1.title = "al haya";
+            b1.pages = 100;
+            object obj = b1;
+            Console.WriteLine(obj); 
+            #endregion
         }
     }
 }
